0868878729945956cf95d9d689b316a1b25a7f77
Dyarhunaity <100211179+Dyarhunaity@users.noreply.github.com>
2022-03-20 20:57:59 -0700

    javatasks

---
+++ sunday20 2/task3/app.js
@@ -0,0 +1,38 @@
+let opreation=prompt(`please enter one of the following operation: +, -, *, /`);
+let first_num_string= prompt(`please enter the first number`);
+let second_num_string= prompt(`please enter the first number`);
+
+
+let first_num_number= number(first_num_string);
+let second_num_number= number(first_num_string);
+
+switch (opreation) {
+    case "+":{
+        let result=first_num_number + second_num_number;
+        console.log(result);
+        alert(result);
+        document.getElementById(`result`).innerHTML=String(result);
+        break;
+    }
+    case "-":{
+        let result=first_num_number - second_num_number;
+        console.log(result);
+        alert(result);
+        document.getElementById(`result`).innerHTML=String(result);
+        
+    }
+    case "*":{
+        let result=first_num_number * second_num_number;
+        console.log(result);
+        alert(result);
+        document.getElementById(`result`).innerHTML=String(result);
+    }
+    case "/":{
+        let result=first_num_number / second_num_number;
+        console.log(result);
+        alert(result);
+        document.getElementById(`result`).innerHTML=String(result);
+    }
+    default:
+        break;
+}
